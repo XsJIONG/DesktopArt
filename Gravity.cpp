3d560b9ce46a95f36dade1bfe067505c482a6395
XsJIONG <250851048@qq.com>
2018-05-13 11:55:53 +0800

    GravityFix Truely!

--- Gravity.cpp
+++ Gravity.cpp
@@ -10,10 +10,13 @@ const double g=9.8;
 const double JUMP=2.0/3.0;
 const int UP=10;
 const int size=50;
+int abs(int a, int b) {
+	return a>b?a-b:b-a;
+}
 struct Item {
 	double x,y,v;
 	bool touch(Item a) {
-		return x,
+		return abs(this->x,a.x)<=size&&abs(this->y,a.y)<=size;
 	}
 }a[1000];
 unsigned long long AL=0;
@@ -46,23 +49,30 @@ int main() {
 	HWND hwndSysListView32=::FindWindowEx(hwndSHELLDLL_DefView, NULL, "SysListView32", "FolderView");
 	Nm = ListView_GetItemCount(hwndSysListView32);
 	int ox=0;
-	int W=GetSystemMetrics(SM_CXFULLSCREEN)-50;
-	int H=GetSystemMetrics(SM_CYFULLSCREEN)-50;
+	int W=GetSystemMetrics(SM_CXFULLSCREEN);
+	int H=GetSystemMetrics(SM_CYFULLSCREEN);
 	double s;
 	s=W/Nm;
 	for(int i=0;i<Nm;i++) a[i].x=i*s,a[i].y=0,a[i].v=0;
+	Item *cur,*che;
 	while (true) {
 		s=W/Nm;
 		for(int i=0;i<Nm;i++) {
-			a[i].x+=AL*a[i].v/2000*GX;
-			a[i].y+=AL*a[i].v/2000*GY;
-			if (a[i].x+size>W||a[i].y+size>H||a[i].x<size||a[i].y<size) a[i].x-=AL*a[i].v/2000*GX,a[i].y-=AL*a[i].v/2000*GY,a[i].v=-a[i].v*JUMP;
-			a[i].v+=UP*g/1000;
+			cur=&a[i];
+			cur->x+=AL*cur->v/2000*GX;
+			cur->y+=AL*cur->v/2000*GY;
 			for (int j=0;j<Nm;j++) {
 				if (i==j) continue;
-				
+				cur=&a[i];
+				che=&a[j];
+				if (cur->touch(*che)) {
+					cur->x-=AL*cur->v/2000*GX,cur->y-=AL*cur->v/2000*GY;
+					if (cur->v>che->v) cur->v=-cur->v*JUMP; else cur->v=cur->v*JUMP;
+				}
 			}
-			::SendMessage(hwndSysListView32, LVM_SETITEMPOSITION, i, MAKELPARAM(a[i].x,a[i].y));
+			cur->v+=UP*g/1000;
+			if (cur->x+size>W||cur->y+size>H||cur->x<0||cur->y<0) cur->x-=AL*cur->v/2000*GX,cur->y-=AL*cur->v/2000*GY,cur->v=-cur->v*JUMP;
+			::SendMessage(hwndSysListView32, LVM_SETITEMPOSITION, i, MAKELPARAM(cur->x,cur->y));
 		}
 		ListView_RedrawItems(hwndSysListView32, 0, ListView_GetItemCount(hwndSysListView32) - 1);
 		::UpdateWindow(hwndSysListView32);
